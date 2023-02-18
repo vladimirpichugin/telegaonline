93ebc2ed8a1c92081516f1bf5ce0dbb9619e96a5
Vladimir Pichugin <vladimir@pichug.in>
2023-02-19 00:11:05 +0300

    Update: add offline func

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import argparse
 import asyncio
+import datetime
 from random import randint
 from telethon import TelegramClient
 from telethon import functions
+from telethon.tl.types import UserStatusOnline, UserStatusOffline
 
 parser = argparse.ArgumentParser(
                     prog='telegaonline',
@@ -17,17 +19,47 @@ args = parser.parse_args()
 client = TelegramClient('main', int(args.api_id), args.api_hash, receive_updates=False)
 
 
+async def update_status(offline):
+    await client(
+        functions.account.UpdateStatusRequest(
+            offline=offline
+        )
+    )
+
+    if offline:
+        t = randint(4, 60 * 3)
+    else:
+        t = randint(9, 34)
+
+    print((f'Offline' if offline else f'Online') + f' {t} seconds..')
+
+    await asyncio.sleep(t)
+
+
+async def now_status():
+    me = await client.get_me()
+    status = me.status
+
+    if type(status) == UserStatusOnline:
+        expires = status.expires + datetime.timedelta(hours=3)
+        return f'Now online, expires: {expires}'
+    if type(status) == UserStatusOffline:
+        was_online = status.was_online + datetime.timedelta(hours=3)
+        return f'Now offline, last seen: {was_online}'
+
+    return 'Unknown status'
+
 async def main():
     while True:
         async with client:
             if client.is_connected():
-                await client(functions.account.UpdateStatusRequest(offline=False))
+                print(await now_status())
+                await update_status(offline=False)
+                await update_status(offline=True)
 
-        t = randint(5, 180)
+                continue
 
-        print(f'Sleep {t} seconds..')
-
-        await asyncio.sleep(t)
+        await asyncio.sleep(5)
 
 
 def stop():
